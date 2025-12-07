f33851122173f787cb8e711febad0adce94bf96b
Luanico <lucasmu07@gmail.com>
2025-12-07 22:14:09 +0100

    refactor: changed some utils function location
    feat: implemented a MLP
    
    feat: implemented a second test file for the MLP

--- matrix_operations.h
+++ matrix_operations.h
@@ -1,7 +1,7 @@
-__global__ void matrix_add(float *A, float *B, float *Result, int width, int height, ssize_t pitch_A, ssize_t pitch_B, ssize_t pitch_Result);
+__global__ void matrix_add(float *A, float *B, float *Result, int width, int height, size_t pitch_A, size_t pitch_B, size_t pitch_Result);
 
-__global__ void matrix_mul(float *A, float *B, float *Result, int m, int n, int p, ssize_t pitch_A, ssize_t pitch_B, ssize_t pitch_Result);
+__global__ void matrix_mul(float *A, float *B, float *Result, int m, int n, int p, size_t pitch_A, size_t pitch_B, size_t pitch_Result);
 
-__global__ void matrix_feedforward(float *X, float *W,float *B, float *Result, int H_in, int H_out, int batch_size, ssize_t pitch_A, ssize_t pitch_W, ssize_t pitch_Result);
+__global__ void matrix_feedforward(float *X, float *W,float *B, float *Result, int H_in, int H_out, int batch_size, size_t pitch_X, size_t pitch_W, size_t pitch_Result);
 
-__global__ void matrix_RELU(float *X, float *Result, int H_in, int batch_size, ssize_t pitch_X, ssize_t pitch_Result);
+__global__ void matrix_RELU(float *X, float *Result, int H_in, int batch_size, size_t pitch_X, size_t pitch_Result);
