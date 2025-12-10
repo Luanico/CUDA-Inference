8dd20c6b2e026e7b1c5b8a2677272d4fd9a3fee7
Baptiste Villeneuve <baptiste.villeneuve@epita.fr>
2025-12-11 00:58:36 +0100

    changed CNN architecture

--- CNN.py
+++ CNN.py
@@ -6,15 +6,31 @@ import time
 class CNN(torch.nn.Module):
     def __init__(self, in_channel, out_channel, input_size, output_size=10):
         super().__init__()
-        self.conv = torch.nn.Conv2d(in_channel, out_channel, 3, padding=1)
-        self.relu = torch.nn.ReLU()
-        self.output = torch.nn.Linear(out_channel * input_size * input_size, output_size)
+        self.conv1 = torch.nn.Conv2d(in_channel, out_channel, 3, padding=1)
+        self.relu1 = torch.nn.ReLU()
+        self.maxpool = torch.nn.MaxPool2d(2, 2)
+        self.conv2 = torch.nn.Conv2d(out_channel, out_channel * 2, 3, padding=1)
+        self.relu2 = torch.nn.ReLU()
+        
+        pooled_size = input_size // 2
+        self.fc1 = torch.nn.Linear(out_channel * 2 * pooled_size * pooled_size, 512)
+        self.relu3 = torch.nn.ReLU()
+        self.fc2 = torch.nn.Linear(512, 256)
+        self.relu4 = torch.nn.ReLU()
+        self.fc3 = torch.nn.Linear(256, output_size)
 
     def forward(self, x):
-        x = self.conv(x)
-        x = self.relu(x)
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.maxpool(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
         x = torch.flatten(x, 1)
-        x = self.output(x)
+        x = self.fc1(x)
+        x = self.relu3(x)
+        x = self.fc2(x)
+        x = self.relu4(x)
+        x = self.fc3(x)
         return x
 
 
